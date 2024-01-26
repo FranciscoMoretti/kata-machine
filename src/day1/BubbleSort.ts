17e704d70a95d302178eba41db80c519cb93ed2e
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-26 10:59:40 +0000

    bubble

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,21 @@
 export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+}
 
+export function bubble_sort_draft(arr: number[]): void {
+    let end = arr.length - 1;
+    while (end > 1) {
+        for (let i = 0; i < end; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+            }
+        }
+        end--;
+    }
 }
