ad9691fd3d13dfc6a6f3aefa5cd80ef901914242
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-27 10:46:06 +0000

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,31 @@
+class Node<T>{
+    public next?: Node<T>
+    public item: T
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>
 
     constructor() {
+        this.head = undefined
+        this.length = 0
     }
 
     push(item: T): void {
-
+        this.head = {item:item, next: this.head}
+        this.length++
 }
     pop(): T | undefined {
-
+        if(!this.head){
+            return undefined
+        }
+        this.length--
+        const item = this.head.item
+        this.head = this.head.next
+        return item
 }
     peek(): T | undefined {
-
+        return this.head?.item
 }
 }
