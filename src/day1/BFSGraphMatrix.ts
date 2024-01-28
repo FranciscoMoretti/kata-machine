41798a82f5634a178161ff49022f79fa851d2992
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-28 18:49:48 +0000

    BFS MAtrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,38 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const numNodes = graph.length
+    
+    const seen = new Array(numNodes).fill(false)
+    const prev = new Array(numNodes).fill(-1)
+    const queue: number[]= []
+    queue.push(source)
+    seen[source] = true
+    do{
+        const curr  = queue.shift() as number
+        if(curr == needle){
+          break 
+        }
+        const adjacents = graph[curr]
+        for(let i=0; i<adjacents.length; i++){
+            if(adjacents[i] === 0 || seen[i]){
+                continue
+            }
 
+            seen[i] = true
+            prev[i] = curr
+            queue.push(i)
+        }
+    }while(queue.length)
+
+    // Build it backwards
+    let curr = needle
+    const out: number[]= []
+
+    while(prev[curr] !== -1){
+        out.push(curr)
+        curr = prev[curr]
+    }
+    if(out.length){
+        return [source].concat(out.reverse())
+    }
+    return null
 }
